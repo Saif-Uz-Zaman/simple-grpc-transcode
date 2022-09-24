eb46c02734f2f3dbd39abc928a556ed377bd9c84
saif_maruf <saif.mektek@gmail.com>
2022-09-25 02:23:19 +0600

    Implemented transaction service

--- makefile
+++ makefile
@@ -1,15 +1,51 @@
 GOOGLEAPIS_DIR = ./googleapis-proto
 APP_PREFIX = simple-grpc-transcode
 DOCKER_REPO = saifmaruf
-RELEASE = 0.0.10
+RELEASE = 0.1.3
 
 
 proto-user:
-	protoc -I${GOOGLEAPIS_DIR} -I. -I/usr/local/include --include_imports --include_source_info --descriptor_set_out=proto/user-grpc-transcode.pd --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative proto/user/user.proto
+	protoc -I${GOOGLEAPIS_DIR} -I. -I/usr/local/include --include_imports --include_source_info --descriptor_set_out=proto/user-grpc-transcode.pd --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative proto/user/user.proto && \
+	kubectl create cm user-rpc-proto-descriptor --from-file proto/user-grpc-transcode.pd -n dev --dry-run=client -o yaml > manifests/user/user-rpc-descriptor-cm.yaml
 
 build-user:
 	GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o container/user/${APP_PREFIX}-user src/user/main.go && cp -r keys container/user
 
 docker-user:
 	docker build -t ${DOCKER_REPO}/${APP_PREFIX}-user:${RELEASE} container/user && \
-	docker push ${DOCKER_REPO}/${APP_PREFIX}-user:${RELEASE}
+	docker push ${DOCKER_REPO}/${APP_PREFIX}-user:${RELEASE} && \
+	yq -y ".spec.template.spec.containers[0].image = \"${DOCKER_REPO}/${APP_PREFIX}-user:${RELEASE}\"" manifests/user/user-deployment.yaml > temp.yaml && \
+	mv temp.yaml manifests/user/user-deployment.yaml
+
+deploy-user:
+	kubectl apply -f manifests/user
+
+
+proto-transaction:
+	protoc -I${GOOGLEAPIS_DIR} -I. -I/usr/local/include --include_imports --include_source_info --descriptor_set_out=proto/transaction-grpc-transcode.pd --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative proto/transaction/transaction.proto && \
+	kubectl create cm transaction-rpc-proto-descriptor --from-file proto/transaction-grpc-transcode.pd -n dev --dry-run=client -o yaml > manifests/transaction/transaction-rpc-descriptor-cm.yaml
+
+build-transaction:
+	GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o container/transaction/${APP_PREFIX}-transaction src/transaction/main.go && cp -r keys container/transaction
+
+docker-transaction:
+	docker build -t ${DOCKER_REPO}/${APP_PREFIX}-transaction:${RELEASE} container/transaction && \
+	docker push ${DOCKER_REPO}/${APP_PREFIX}-transaction:${RELEASE} && \
+	yq -y ".spec.template.spec.containers[0].image = \"${DOCKER_REPO}/${APP_PREFIX}-transaction:${RELEASE}\"" manifests/transaction/transaction-deployment.yaml > temp.yaml && \
+	mv temp.yaml manifests/transaction/transaction-deployment.yaml
+
+deploy-transaction:
+	kubectl apply -f manifests/transaction
+
+clean:
+	find ./proto -regex ".*\.go\|.*\.pd"|xargs rm
+	rm -f container/user/${APP_PREFIX}-user
+	rm -f container/transaction/${APP_PREFIX}-transaction
+
+deploy-pg: 
+	kubectl apply -f manifests/postgres.yaml
+
+proto-all: proto-user proto-transaction
+build-all: build-user build-transaction
+docker-all: docker-user docker-transaction
+deploy-all: deploy-user deploy-transaction deploy-pg
--- src/transaction/main.go
+++ src/transaction/main.go
@@ -1,5 +1,114 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	protoTransaction "simple-grpc-transcode/proto/transaction"
+	tokenutill "simple-grpc-transcode/src/user/token-utill"
 
+	"github.com/jackc/pgx/v4"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":8082"
+)
+
+type TransactionGrpcServerImpl struct {
+	conn *pgx.Conn
+	protoTransaction.UnimplementedTransactionManagementServer
+}
+
+var (
+	grpcServerImpl *TransactionGrpcServerImpl                   = &TransactionGrpcServerImpl{}
+	_              protoTransaction.TransactionManagementServer = grpcServerImpl
+)
+
+func CreateTransaction(tgsi *TransactionGrpcServerImpl, in *protoTransaction.UpdateTxRequest, tx_status string, current_balance int32) {
+	createSql := `
+	create table if not exists transaction_records(
+		user_id int,
+		amount int,
+		debited_or_credited text,
+		current_balance int
+	);
+	`
+	_, err := tgsi.conn.Exec(context.Background(), createSql)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Table creation failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	created_transaction := &protoTransaction.UpdateTxRequest{Id: in.Id, Amount: in.Amount}
+	tx, err := tgsi.conn.Begin(context.Background())
+	if err != nil {
+		log.Fatalf("conn.Begin Failed: %v", err)
+	}
+
+	statementSql := `
+	insert into transaction_records(user_id, amount, debited_or_credited, current_balance) 
+	values ($1, $2, $3, $4);
+	`
+	_, err = tx.Exec(context.Background(), statementSql, created_transaction.Id, created_transaction.Amount, tx_status, current_balance)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query. %v", err)
+	}
+	tx.Commit(context.Background())
+}
+
+func (tgsi *TransactionGrpcServerImpl) UpTransaction(ctx context.Context, in *protoTransaction.UpdateTxRequest) (*protoTransaction.UpdateTxResponse, error) {
+	log.Printf("Received Id: %v", in.Id)
+	current_balance := int32(200)
+	CreateTransaction(tgsi, in, "credited", current_balance)
+	response := &protoTransaction.UpdateTxResponse{Balance: current_balance}
+	return response, nil
+}
+
+func (tgsi *TransactionGrpcServerImpl) DownTransaction(ctx context.Context, in *protoTransaction.UpdateTxRequest) (*protoTransaction.UpdateTxResponse, error) {
+	log.Printf("Received Id: %v", in.Id)
+	current_balance := int32(200)
+	CreateTransaction(tgsi, in, "debited", current_balance)
+	response := &protoTransaction.UpdateTxResponse{Balance: current_balance}
+	return response, nil
+}
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
+func main() {
+	fmt.Printf("%s\n", tokenutill.GenerateJWK())
+	// pgsql connection
+	database_url := getenv("POSTGRES_CONN_STRING", "postgres://transactiondb:transactiondb@localhost:5432/transactiondb")
+	conn, err := pgx.Connect(context.Background(), database_url)
+	if err != nil {
+		log.Fatalf("Unable to establish connection: %v", err)
+	}
+	defer conn.Close(context.Background())
+	log.Printf("Connection established")
+
+	// grpc server
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	log.Printf("Grpc server listening on port: %s", port)
+	grpcSrv := grpc.NewServer()
+	grpcServerImpl.conn = conn
+	protoTransaction.RegisterTransactionManagementServer(grpcSrv, grpcServerImpl)
+
+	reflection.Register(grpcSrv)
+	if err := grpcSrv.Serve(listen); err != nil {
+		log.Fatalf("Failed to serve")
+	}
 }
